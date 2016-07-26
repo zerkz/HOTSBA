334aadb64c2b10da3b82e6a6c575d8272bc66a21
aleontiev <alonetiev@gmail.com>
2016-07-26 13:24:37 -0400

    test descriptions

--- test/models/player.js
+++ test/models/player.js
@@ -20,28 +20,28 @@ describe('PlayerModel', function() {
       return expect(playerModel.getPlayerIdByName(MY_PLAYER_NAME))
         .to.eventually.equal(MY_HOTSLOGS_PLAYER_ID);
     });
-    it("should always get my own playerId from my friend's player name.", function() {
+    it("should always get my friend's playerId from my friend's player name.", function() {
       return expect(playerModel.getPlayerIdByName("imnasti"))
         .to.eventually.equal('1978756');
     });
   });
   describe("#getTopHeroesByPlayerId", function () {
-    it("should always get 5 heroes from by my own playerId.", function() {
+    it("should always get 5 heroes from my own playerId.", function() {
       return expect(playerModel.getTopHeroesByPlayerId(QUERY_PARAMS.limit, QUERY_PARAMS.sort)(MY_HOTSLOGS_PLAYER_ID))
         .to.eventually.have.length(HEROES_LIMIT);
     });
   });
   describe("#getPlayerIdByBattleTag", function () {
-    it("should always get my player ID from my own battletag..", function() {
+    it("should always get my player ID from my own battletag.", function() {
       return expect(playerModel.getPlayerIdByBattleTag(MY_BATTLETAG, regionModel.REGIONS.US)).to.eventually.equal(parseInt(MY_HOTSLOGS_PLAYER_ID));
     });
   });
   describe("#getTopPlayedHeroesByPlayerNameOrBattleTag", function () {
-    it("should always get 5 heroes from by my own player name.", function() {
+    it("should always get 5 heroes from my own player name.", function() {
       return expect(playerModel.getTopPlayedHeroesByPlayerNameOrBattleTag(MY_PLAYER_NAME, QUERY_PARAMS))
         .to.eventually.have.length(HEROES_LIMIT);
     });
-    it("should always get 5 heroes from by my own player battletag.", function() {
+    it("should always get 5 heroes from my own player battletag.", function() {
       return expect(playerModel.getTopPlayedHeroesByPlayerNameOrBattleTag(MY_BATTLETAG, QUERY_PARAMS))
         .to.eventually.have.length(HEROES_LIMIT);
     });
