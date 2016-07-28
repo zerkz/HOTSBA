d57f52c8489f0cb1a42fd87cb8ac23e3ae86ae50
aleontiev <alonetiev@gmail.com>
2016-07-27 22:39:57 -0400

    fix tests

--- test/models/hero_images.js
+++ test/models/hero_images.js
@@ -2,5 +2,4 @@ let HeroImagesModel = require('../../models/hero_images');
 let expect = require('chai').expect;
 
 describe('HeroImagesModel', function() {
-  });
 });
--- test/models/player.js
+++ test/models/player.js
@@ -10,7 +10,7 @@ const MY_HOTSLOGS_PLAYER_ID = 2377102;
 const HEROES_LIMIT = 5;
 const QUERY_PARAMS = {
   limit: HEROES_LIMIT,
-  sort: playerModel.HOTS_LOGS_DEFAULT_SORT,
+  sort: PlayerModel.HOTS_LOGS_DEFAULT_SORT,
   region: RegionModel.REGIONS.US
 };
 
@@ -21,28 +21,28 @@ describe('PlayerModel', function() {
         .to.eventually.equal(MY_HOTSLOGS_PLAYER_ID);
     });
     it("should always get my friend's playerId from my friend's player name.", function() {
-      return expect(playerModel.getPlayerIdByName("imnasti"))
+      return expect(PlayerModel.getPlayerIdByName("imnasti"))
         .to.eventually.equal('1978756');
     });
   });
   describe("#getTopHeroesByPlayerId", function () {
     it("should always get 5 heroes from my own playerId.", function() {
-      return expect(playerModel.getTopHeroesByPlayerId(QUERY_PARAMS.limit, QUERY_PARAMS.sort)(MY_HOTSLOGS_PLAYER_ID))
+      return expect(PlayerModel.getTopHeroesByPlayerId(QUERY_PARAMS.limit, QUERY_PARAMS.sort)(MY_HOTSLOGS_PLAYER_ID))
         .to.eventually.have.length(HEROES_LIMIT);
     });
   });
   describe("#getPlayerIdByBattleTag", function () {
     it("should always get my player ID from my own battletag.", function() {
-      return expect(playerModel.getPlayerIdByBattleTag(MY_BATTLETAG, RegionModel.REGIONS.US)).to.eventually.equal(parseInt(MY_HOTSLOGS_PLAYER_ID));
+      return expect(PlayerModel.getPlayerIdByBattleTag(MY_BATTLETAG, RegionModel.REGIONS.US)).to.eventually.equal(parseInt(MY_HOTSLOGS_PLAYER_ID));
     });
   });
   describe("#getTopPlayedHeroesByPlayerNameOrBattleTag", function () {
     it("should always get 5 heroes from my own player name.", function() {
-      return expect(playerModel.getTopPlayedHeroesByPlayerNameOrBattleTag(MY_PLAYER_NAME, QUERY_PARAMS))
+      return expect(PlayerModel.getTopPlayedHeroesByPlayerNameOrBattleTag(MY_PLAYER_NAME, QUERY_PARAMS))
         .to.eventually.have.length(HEROES_LIMIT);
     });
     it("should always get 5 heroes from my own player battletag.", function() {
-      return expect(playerModel.getTopPlayedHeroesByPlayerNameOrBattleTag(MY_BATTLETAG, QUERY_PARAMS))
+      return expect(PlayerModel.getTopPlayedHeroesByPlayerNameOrBattleTag(MY_BATTLETAG, QUERY_PARAMS))
         .to.eventually.have.length(HEROES_LIMIT);
     });
   });
