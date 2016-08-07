809813d01a7b3e9851ae1caba9b942fde53a0c74
Zack Whipkey <zack.whipkey@gmail.com>
2016-08-07 10:21:23 -0500

    Add clear all functionality, fix hogan templates to not be such a pain to change, start on configuration mechanism

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-const {app, BrowserWindow} = require('electron');
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+const config = require('convict');
 
 let win;
+let devMode = true;
 
-function createMainWindow() {
-  win = new BrowserWindow({width: 800, height: 600})
-  win.loadURL(`file://${__dirname}/index.html`)
-  win.webContents.openDevTools()
+
+app.on('ready', createMainWindow);
+
+function createMainWindow(width, height) {
+  let primaryDisplay = electron.screen.getPrimaryDisplay();
+  console.log(primaryDisplay);
+  win = new BrowserWindow({width: width, height: height, show : false})
+  win.loadURL(`file://${__dirname}/index.html`);
+  if (devMode) {
+    win.webContents.openDevTools();
+  }
+  win.once('ready-to-show', () => {
+    win.show()
+  });
 }
 
-app.on('ready', createMainWindow)
+app.on('will-quit', saveConfigs);
+
+function saveConfigs () {
+  //save screen stuff to configs before closing.
+}
